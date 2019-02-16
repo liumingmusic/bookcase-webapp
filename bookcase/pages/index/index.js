3e9213c47d67bde06fb0d11ba9dc9fc640589fe8
liumingmusic <liuming_music@163.com>
2019-02-16 22:41:04 +0800

    图书删除功能和用户登录体验功能
    图书删除功能和用户登录体验功能

--- bookcase/pages/index/index.js
+++ bookcase/pages/index/index.js
@@ -1,4 +1,5 @@
 const app = getApp();
+import Dialog from '../../vant/dialog/dialog';
 
 Page({
 
@@ -21,37 +22,7 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function() {
-    //准备成功
-    this.setData({
-      onReady: true
-    });
-    // 获取用户信息
-    wx.getSetting({
-      success: function(res) {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: function(res) {
-              //全局
-              app.globalData.avatarUrl = res.userInfo.avatarUrl;
-              app.globalData.userInfo = res.userInfo;
-              app.globalData.logged = true;
-              // 调用云函数
-              wx.cloud.callFunction({
-                name: 'login',
-                success: function(res) {
-                  app.globalData.openid = res.result.openid
-                }
-              });
-            }
-          }, 3000)
-        } else {
-          wx.navigateTo({
-            url: '../getUserInfo/getUserInfo'
-          });
-        }
-      }
-    });
+
   },
 
   /**
@@ -60,10 +31,36 @@ Page({
   onShow: function() {
     let _that = this;
     if (app.globalData.logged) {
-      this.getBookList();
-    } else if (_that.data.onReady || !app.globalData.logged) {
-      wx.navigateTo({
-        url: '../getUserInfo/getUserInfo'
+      _that.getBookList();
+    } else {
+      // 获取用户信息
+      wx.getSetting({
+        success: function(res) {
+          if (res.authSetting['scope.userInfo']) {
+            // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+            wx.getUserInfo({
+              success: function(res) {
+                //全局
+                app.globalData.avatarUrl = res.userInfo.avatarUrl;
+                app.globalData.userInfo = res.userInfo;
+                app.globalData.logged = true;
+                // 调用云函数
+                wx.cloud.callFunction({
+                  name: 'login',
+                  success: function(res) {
+                    app.globalData.openid = res.result.openid;
+                    //再次获取图书列表
+                    _that.getBookList();
+                  }
+                });
+              }
+            }, 3000)
+          } else {
+            wx.navigateTo({
+              url: '../getUserInfo/getUserInfo'
+            });
+          }
+        }
       });
     }
   },
@@ -117,5 +114,51 @@ Page({
         });
       }
     });
+  },
+
+  /**
+   * 删除数据
+   */
+  removeById: function(event) {
+    let bookId = event.target.dataset.bookid;
+    let _that = this;
+    Dialog.confirm({
+      title: "警告",
+      message: "是否删除图书"
+    }).then(() => {
+      const db = wx.cloud.database();
+      db.collection('todo_collection').doc(bookId).remove({
+        success: function() {
+          wx.showToast({
+            title: '删除成功',
+            icon: 'success',
+            duration: 2000
+          });
+          //删除数组中的列表
+          let index = _that.data.bookList.findIndex(function(item) {
+            return item._id === bookId
+          });
+          _that.data.bookList.splice(index, 1);
+          _that.setData({
+            bookList: _that.data.bookList
+          })
+        },
+        fail: function() {
+          console.log("云存储删除失败");
+        }
+      })
+    }).catch(() => {
+      console.log("删除失败");
+    });
+  },
+
+  /**
+   * 查看数据
+   */
+  findById: function(event) {
+    let bookItem = event.target.dataset.bookitem;
+    console.log(bookItem);
   }
+
+
 })
